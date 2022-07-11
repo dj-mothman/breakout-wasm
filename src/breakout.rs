75443b805ad28b9a33b0e5816ce68b4b4ec5a52c
Jacob <jacobforbes@talktalk.net>
2022-07-11 02:01:46 +0100

    More UI & collision is broken

--- src/breakout.rs
+++ src/breakout.rs
@@ -1,14 +1,14 @@
 use std::{collections::{VecDeque}, fmt};
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub struct Position {
 	pub x: i8,
 	pub y: i8,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub struct Ball {
-	position: Position,
+	pub position: Position,
 	x_vel: i8,
 	y_vel: i8,
 }
@@ -34,9 +34,16 @@ pub enum Color {
 
 impl fmt::Display for Color {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-			write!(f, "{:?}", self)
-			// or, alternatively:
-			// fmt::Debug::fmt(self, f)
+		let emoji = match self {
+			Color::Red => '🟥',
+			Color::Green => '🟩',
+			Color::Blue => '🟦',
+			Color::Yellow => '🟨',
+		};
+
+		write!(f, "{}", emoji)
+		// or, alternatively:
+		// fmt::Debug::fmt(self, f)
 	}
 }
 
@@ -89,9 +96,9 @@ impl BreakoutGame {
 		let end = (width/2)+PADDLE_EXTRA;
 
 		Self {
-			ball: Ball{position: Position{x: (width/2) as i8, y: (height-1) as i8}, x_vel: 0, y_vel: 0},
+			ball: Ball{position: Position{x: (width/2) as i8, y: (height-2) as i8}, x_vel: 0, y_vel: 0},
 			blocks: create_blocks(),
-			player: (start..=end).map(|x| Position{x: x as i8, y: height as i8}).collect(),
+			player: (start..=end).map(|x| Position{x: x as i8, y: (height-1) as i8}).collect(),
 			width: width,
 			height: height,
 		}
@@ -102,20 +109,70 @@ impl BreakoutGame {
 		let back = self.player.get(2*PADDLE_EXTRA as usize).unwrap().x;
 
 		match direction {
-			Direction::Left => if self.ball.x_vel == 0 && self.ball.y_vel == 0 {
-				self.ball.x_vel = -1;
-				self.ball.y_vel = -1;
-			} else if front > 0 {
-				self.player.pop_back();
-				self.player.push_front(Position{x:front-1, y:self.height as i8});
+			Direction::Left => {
+				if self.ball.x_vel == 0 && self.ball.y_vel == 0 {
+					self.ball.x_vel = -1;
+					self.ball.y_vel = -1;
+				}
+				if front > 0 {
+					self.player.pop_back();
+					self.player.push_front(Position{x:front-1, y:(self.height-1) as i8});
+				}
 			},
-			Direction::Right => if self.ball.x_vel == 0 && self.ball.y_vel == 0 {
-				self.ball.x_vel = 1;
-				self.ball.y_vel = -1;
-			} else if back < self.width as i8 {
-				self.player.pop_front();
-				self.player.push_back(Position{x:back+1, y:self.height as i8});
+			Direction::Right => {
+				if self.ball.x_vel == 0 && self.ball.y_vel == 0 {
+					self.ball.x_vel = 1;
+					self.ball.y_vel = -1;
+				}
+				if back < (self.width-1) as i8 {
+					self.player.pop_front();
+					self.player.push_back(Position{x:back+1, y:(self.height-1) as i8});
+				}
 			},
+		}
+	}
+
+	fn get_positions_to_check(&self, next_ball_pos: Position) -> Vec<Position> {
+		let mut positions_to_check = Vec::new();
+		if self.ball.x_vel > 0 { 
+			positions_to_check.push(Position {x: self.ball.position.x + 1, y: self.ball.position.y})
+		} else if self.ball.x_vel < 0 {			
+			positions_to_check.push(Position {x: self.ball.position.x - 1, y: self.ball.position.y})
+		}
+		if self.ball.y_vel > 0 { 
+			positions_to_check.push(Position {x: self.ball.position.x, y: self.ball.position.y + 1})
+		} else if self.ball.x_vel < 0 {			
+			positions_to_check.push(Position {x: self.ball.position.x, y: self.ball.position.y - 1})
+		}		
+		positions_to_check.push(next_ball_pos);
+		positions_to_check
+	}
+
+	fn check_positions(&mut self, positions_to_check: Vec<Position>, mut next_ball_pos: Position) {
+		for p in 0..positions_to_check.len() {
+			let check_pos = positions_to_check.get(p).unwrap();
+			for i in 0..self.blocks.len() - 1 {
+				if self.blocks[i].positions.iter().any(|p| check_pos.x == p.x && check_pos.y == p.y) {
+	
+					if self.blocks[i].positions[0].x > self.ball.position.x || self.blocks[i].positions[self.blocks[i].positions.len() - 1].x < self.ball.position.x {					
+						self.ball.x_vel = -self.ball.x_vel;
+						next_ball_pos.x = self.ball.position.x + self.ball.x_vel;
+					}
+	
+					if self.blocks[i].positions[0].y != self.ball.position.y {					
+						self.ball.y_vel = -self.ball.y_vel;
+						next_ball_pos.y = self.ball.position.y + self.ball.y_vel;
+					}
+	
+					println!("removed: {:?}", self.blocks[i]);
+					self.blocks.remove(i);
+
+					let new_pos_to_check = self.get_positions_to_check(next_ball_pos);
+					self.check_positions(new_pos_to_check, next_ball_pos);
+
+					return;
+				}
+			}
 		}
 	}
 
@@ -125,7 +182,7 @@ impl BreakoutGame {
 			y: self.ball.position.y + self.ball.y_vel,
 		};
 
-		if next_ball_pos.x < 0 || next_ball_pos.x > self.width as i8 {
+		if next_ball_pos.x < 0 || next_ball_pos.x >= self.width as i8 {
 			self.ball.x_vel = -self.ball.x_vel;
 			next_ball_pos.x = self.ball.position.x + self.ball.x_vel;
 		}
@@ -134,33 +191,21 @@ impl BreakoutGame {
 			self.ball.y_vel = -self.ball.y_vel;
 			next_ball_pos.y = self.ball.position.y + self.ball.y_vel;
 		}
-		else if next_ball_pos.y > self.height as i8 {
+		else if next_ball_pos.y >= self.height as i8 {
 			println!("game over");
 		}
 
-		for i in 0..self.blocks.len() - 1 {
-			if self.blocks[i].positions.iter().any(|p| next_ball_pos.x == p.x && next_ball_pos.y == p.y) {
-
-				if self.blocks[i].positions[0].x > self.ball.position.x || self.blocks[i].positions[self.blocks[i].positions.len() - 1].x < self.ball.position.x {					
-					self.ball.x_vel = -self.ball.x_vel;
-					next_ball_pos.x = self.ball.position.x + self.ball.x_vel;
-				}
-
-				if self.blocks[i].positions[0].y > self.ball.position.y || self.blocks[i].positions[self.blocks[i].positions.len() - 1].y < self.ball.position.y {					
-					self.ball.y_vel = -self.ball.y_vel;
-					next_ball_pos.y = self.ball.position.y + self.ball.y_vel;
-				}
-
-				println!("removed: {:?}", self.blocks[i]);
-				self.blocks.remove(i);
-			}
-		}
-		
+		let positions_to_check = self.get_positions_to_check(next_ball_pos);
+		self.check_positions(positions_to_check, next_ball_pos);		
 		self.ball.position = next_ball_pos;		
 	}
 
 	pub fn pos_iter(&self) -> impl Iterator<Item = Position> + '_ {
 		return (0..self.height).flat_map(move |y| (0..self.width).map(move |x| Position {x: x as i8, y: y as i8}))
+	}
+
+	pub fn tick(&mut self) {
+		self.move_ball();
 	}
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
-use js_sys::Reflect;
-use breakout::BreakoutGame;
-use wasm_bindgen::JsValue;
-use wasm_react::{c, h, Component, hooks::use_state, export_components, props::Style};
+use js_sys::{Reflect, Function};
+use breakout::{BreakoutGame, Direction};
+use wasm_bindgen::{JsValue, UnwrapThrowExt, prelude::Closure, JsCast};
+use wasm_react::{c, h, Component, hooks::{use_state, use_effect, Deps, use_callback}, export_components, props::Style};
+use web_sys::{window, KeyboardEvent};
 
 mod breakout;
 
@@ -25,20 +26,76 @@ impl Component for App {
 	fn render(&self) -> wasm_react::VNode {
 		let game = use_state(|| BreakoutGame::new(self.width, self.height));
 
+		use_effect({
+			let game = game.clone();
+			move || {
+				let tick_closure = Closure::new({
+					let mut game = game.clone();
+					move || {
+						game.set(|mut game| {
+							game.tick();
+							game
+						})
+					}
+				});
+
+			let handle = window().unwrap_throw().set_interval_with_callback_and_timeout_and_arguments_0(
+				tick_closure.as_ref().dyn_ref::<Function>().unwrap_throw(),
+				250)
+				.unwrap_throw();
+
+			move || {
+				drop(tick_closure);
+				window().unwrap_throw().clear_interval_with_handle(handle);
+			}
+		}}, Deps::none());
+
+		let handle_key_down = use_callback({
+			let mut game = game.clone();
+			move |evt: KeyboardEvent| {			
+			let direction = match evt.code().as_ref() {
+				"ArrowLeft" => Some(Direction::Left),
+				"ArrowRight" => Some(Direction::Right),
+				_ => None,
+			};
+
+			if let Some(direction) = direction {
+				game.set(|mut game| {
+					game.move_paddle(direction);
+					game
+				})
+			}
+		}}, Deps::none());
+
 		h!(div)
+			.tabindex(0)
+			.autofocus(true)
+			.on_keydown(&handle_key_down)
 			.style(&Style::new()
 				.display("inline-grid")
 				.grid_template(format!("repeat({}, 1em) / repeat({}, 1em)", self.height, self.width))
 				.border("1px solid grey"))
 			.build(c![
 			..game.value().pos_iter().map(|pos| {
+				let ball = &game.value().ball;
+				if pos.x == ball.position.x && pos.y == ball.position.y {
+					return h!(div).style(&Style::new().text_indent("-.2em").margin_top("-.2em")).build(c!["⚫"])
+				} 
+
 				for b in 0..game.value().blocks.len() {
 					let block = &game.value().blocks[b];
 					for p in 0..block.positions.len() {
 						let block_pos = &block.positions[p];
 						if pos.x == block_pos.x as i8 && pos.y == block_pos.y as i8 {
-							return h!(div).build(c![block.color.to_string().chars().next().unwrap().to_string()])
+							return h!(div).style(&Style::new().text_indent("-.2em").margin_top("-.2em")).build(c![block.color.to_string()])
 						}
+					}
+				}
+
+				for p in 0..game.value().player.len() {
+					let player_block = &game.value().player[p];
+					if pos.x == player_block.x && pos.y == player_block.y {
+						return h!(div).style(&Style::new().text_indent("-.2em").margin_top("-.2em")).build(c!["⬛"])
 					}
 				}
 
